9a810643c70e605ca676c3cc132de8c33621207e
Dan <dan@tropity.com>
2021-04-08 16:46:53 +0100

    Update website-config.js

--- src/website-config.js
+++ src/website-config.js
@@ -2,10 +2,24 @@
  *  ---------------------------------------
  *  | Copyright 2021 by https://da-xn.com |
  *  ---------------------------------------
+ *  |             V1 . 0 . 0              |
+ *  ---------------------------------------
 */
 
 serverSite.setup({
-    "website-title": "Tropity Server - Welcome",
+    "website-title": "Tropity - Discord Server",
+    "header-links": {
+        "Home": "/",
+        "Invite Link": "https://discord.gg/D6Khj5wufX"
+    },
+    "website-main-text": "",
+    "website-main-description": "",
     "discord-server-name": "Tropity",
-    "discord-server-invite": "https://discord.gg/XF5ft6QR8A"
+    "discord-server-invite": "https://discord.gg/D6Khj5wufX",
+    "features": {
+        "Test": "Test",
+        "Test": "Test",
+        "Test": "Test"
+    },
+    "discord-server-owner": "Dann#0001"
 });
