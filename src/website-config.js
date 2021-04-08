981b8d2188f26fcec0371b31ef689913574bbc2d
Dan <dan@tropity.com>
2021-04-08 19:33:23 +0100

    Update website-config.js

--- src/website-config.js
+++ src/website-config.js
@@ -10,16 +10,21 @@ serverSite.setup({
     "website-title": "Tropity - Discord Server",
     "header-links": {
         "Home": "/",
-        "Invite Link": "https://discord.gg/D6Khj5wufX"
+        "Invite Link": "https://discord.gg/D6Khj5wufX",
     },
-    "website-main-text": "",
-    "website-main-description": "",
+    "website-main-text": "Welcome to the Tropity Server!",
+    "website-main-description": "We have an amazing community and lots of fun channels in our server!",
     "discord-server-name": "Tropity",
     "discord-server-invite": "https://discord.gg/D6Khj5wufX",
     "features": {
-        "Test": "Test",
-        "Test": "Test",
-        "Test": "Test"
+        "Great Members": "We have an amazing community with many members. You will have lots of fun!",
+        "Lots of Bots": "We provide lots of bots to our community. Join today to find out what they can do and try and beat our records!",
+        "Lots of channels": "Have fun in the many channels we provide. From general chats to advertisement channels!"
     },
-    "discord-server-owner": "Dann#0001"
+    "discord-server-owner": "Dann#0001",
+    "statistics": {
+        "Users": "020",
+        "Bots": "005",
+        "Channels": "100",
+    }
 });
