0fece293c03e328317a1ec953f92ed760dc15075
Dan <dan@tropity.com>
2021-04-08 02:12:46 +0100

    Create website-config.js

---
+++ src/website-config.js
@@ -0,0 +1,11 @@
+/**
+ *  ---------------------------------------
+ *  | Copyright 2021 by https://da-xn.com |
+ *  ---------------------------------------
+*/
+
+serverSite.setup({
+    "website-title": "Tropity Server - Welcome",
+    "discord-server-name": "Tropity",
+    "discord-server-invite": "https://discord.gg/XF5ft6QR8A"
+});
